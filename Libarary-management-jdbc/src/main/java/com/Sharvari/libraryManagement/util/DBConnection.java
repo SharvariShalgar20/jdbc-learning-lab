fd5f789e57ee863c704c2010e38d6f590f778d74
SharvariShalgar20 <shalgarsharvari@gmail.com>
2026-03-20 14:28:44 +0530

    feat : DBConnection.java implemented

---
+++ Libarary-management-jdbc/src/main/java/com/Sharvari/libraryManagement/util/DBConnection.java
@@ -0,0 +1,23 @@
+package com.Sharvari.libraryManagement.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/library_db";
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "Sharvarishalgar@2005";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+}
