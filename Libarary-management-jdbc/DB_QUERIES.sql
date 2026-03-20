79c0bbf6449f20ff97ad104da0f5db3e1e620f6f
SharvariShalgar20 <shalgarsharvari@gmail.com>
2026-03-20 14:08:52 +0530

    feat : dependencies added and DB queries added

---
+++ Libarary-management-jdbc/DB_QUERIES.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE library_db;
+
+USE library_db;
+
+CREATE TABLE books (
+                       id INT PRIMARY KEY AUTO_INCREMENT,
+                       title VARCHAR(100),
+                       author VARCHAR(100),
+                       available BOOLEAN DEFAULT TRUE
+);
