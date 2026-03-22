918e789c371be31f8b5be76e170c107083e960bd
SharvariShalgar20 <shalgarsharvari@gmail.com>
2026-03-22 19:19:27 +0530

    model layer implemented

--- Student-Enrollment-Portal/src/main/java/com/enrollment/model/Course.java
+++ Student-Enrollment-Portal/src/main/java/com/enrollment/model/Course.java
@@ -1,4 +1,32 @@
 package com.enrollment.model;
 
 public class Course {
+
+    private int id;
+    private String title;
+    private int credits;
+    private int max_capacity;
+
+    public Course(int id, String title, int credits, int max_capacity) {
+        this.id = id;
+        this.title = title;
+        this.credits = credits;
+        this.max_capacity = max_capacity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public int getMax_capacity() {
+        return max_capacity;
+    }
 }
